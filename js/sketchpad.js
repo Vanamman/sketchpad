b04ac0432d87cf80aa92f4d403ad56f78af96422
Vanamman <vanamman06@gmail.com>
2017-06-04 20:31:56 -0500

    Added css, div grid, and hover class for grid

---
+++ js/sketchpad.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	for (var j = 0; j < 16; j++) {
+		for (var i = 0; i <= 15; i++) {
+			$('<div></div').appendTo('#container');
+
+		}
+	}
+
+	$('div').hover(function() {
+
+		$(this).addClass('highlighted');
+	}, function() {
+
+		$(this).removeClass('highlighted');
+	})
+})
