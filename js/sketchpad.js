b7093fbd96a05a67936c48957c6d01c03fe023d3
Vanamman <vanamman06@gmail.com>
2017-06-07 19:36:50 -0500

    added buttons to clear and resize grid

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,17 +1,39 @@
-$(document).ready(function() {
+function grid(gridSize) {
 
-	for (var j = 0; j < 16; j++) {
-		for (var i = 0; i <= 15; i++) {
-			$('<div></div').appendTo('#container');
+	var containerSize = 500 / gridSize;
 
+	$('#container').empty();
+
+	for (var j = 1; j <= gridSize; j++) {
+
+		for (var i = 2; i <= gridSize + 1; i++) {
+			$('<div class="cell"></div>').appendTo('#container');
 		}
 	}
 
-	$('div').hover(function() {
+	$('.cell').hover(function() {
 
 		$(this).addClass('highlighted');
-	}, function() {
+	})
 
-		$(this).removeClass('highlighted');
+	$('.cell').width(containerSize);
+	$('.cell').height(containerSize);
+}
+
+
+$(document).ready(function() {
+	grid(16);  //starting grid size
+
+	$('#clear').on('click', function () {
+
+		$('.cell').removeClass('highlighted');
+	})
+	
+
+	$('button#new').click(function () {
+		$('.cell').removeClass('highlighted');
+		var newSize = prompt('How big would you like the grid to be?' );
+		grid(newSize);
+
 	})
 })
